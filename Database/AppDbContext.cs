fa7ff5e620c15265bad9d5930091068782fce74c
Angela <yaoliu928@gmail.com>
2026-09-20 14:34:41 +1000

    add data

--- Database/AppDbContext.cs
+++ Database/AppDbContext.cs
@@ -2,6 +2,9 @@ namespace Trip.API.Database;
 
 using Microsoft.EntityFrameworkCore;
 using Trip.API.Models;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
 
 public class AppDbContext : DbContext
 {
@@ -12,4 +15,28 @@ public class AppDbContext : DbContext
   public DbSet<TouristRoute> TouristRoutes { get; set; }
 
   public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+{
+    // modelBuilder.Entity<TouristRoute>()
+    //     .HasData(new TouristRoute
+    //     {
+    //         Id = Guid.Parse("6f9619ff-8b86-d011-b42d-00c04fc964ff"),
+    //         Title = "test title",
+    //         Description = "This is a sample description for the tourist route.",
+    //         OriginalPrice = 0,
+    //         CreateTime = new DateTime(
+    //             2026, 9, 20, 0, 0, 0,
+    //             DateTimeKind.Utc)
+    //     });
+
+            var touristRouteJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutesMockData.json");
+            IList<TouristRoute> touristRoutes = JsonConvert.DeserializeObject<IList<TouristRoute>>(touristRouteJsonData);
+            modelBuilder.Entity<TouristRoute>().HasData(touristRoutes);
+
+            var touristRoutePictureJsonData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"/Database/touristRoutePicturesMockData.json");
+            IList<TouristRoutePicture> touristRoutePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(touristRoutePictureJsonData);
+            modelBuilder.Entity<TouristRoutePicture>().HasData(touristRoutePictures);
+    base.OnModelCreating(modelBuilder);
+}
 }
---
+++ Migrations/20260920040311_DataSeeding.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trip.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristRoutes",
+                columns: new[] { "Id", "CreateTime", "DepartureTime", "Description", "DiscountPercent", "Features", "Fees", "Notes", "OriginalPrice", "Title", "UpdateTime" },
+                values: new object[] { new Guid("6f9619ff-8b86-d011-b42d-00c04fc964ff"), new DateTime(2026, 9, 20, 0, 0, 0, 0, DateTimeKind.Utc), null, "This is a sample description for the tourist route.", null, null, null, null, 0m, "test title", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristRoutes",
+                keyColumn: "Id",
+                keyValue: new Guid("6f9619ff-8b86-d011-b42d-00c04fc964ff"));
+        }
+    }
+}
